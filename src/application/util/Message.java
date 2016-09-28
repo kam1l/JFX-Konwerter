e5c574e8f8312f4f47a25244219fa9a968ec9c93
Kamil Oleksik <kamiloleksik@gmail.com>
2016-09-28 13:32:15 +0200

    Change message constant

--- src/application/util/Message.java
+++ src/application/util/Message.java
@@ -8,8 +8,8 @@ public class Message
 	public static final String ERROR_TITLE = "B³¹d";
 	public static final String INFORMATION_TITLE = "Informacja";
 
-	public static final String CRITICAL_ERROR_MESSAGE = "Wyst¹pi³ b³¹d podczas odczytu danych i aplikacja nie "
-			+ "mo¿e kontynuowaæ dzia³ania.";
+	public static final String CRITICAL_ERROR_MESSAGE = "Wyst¹pi³ b³¹d podczas odczytu pliku bazy danych "
+			+ "i aplikacja nie mo¿e kontynuowaæ dzia³ania.";
 	public static final String UPDATE_ERROR_MESSAGE = "Wyst¹pi³ b³¹d podczas aktualizacji kursów walut.";
 	public static final String UPDATE_SUCCESS_MESSAGE = "Aktualizacja kursów walut przebieg³a pomyœlnie.";
 	public static final String SAVING_PREFERENCES_ERROR_MESSAGE = "Wyst¹pi³ b³¹d podczas zapisu ustawieñ.";
